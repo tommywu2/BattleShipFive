1c5f0895587be1f8778497c7b5d00076604d3f03
Tommy Wu <tommy.wu2@education.nsw.gov.au>
2023-03-24 12:41:46 +1100

    Add project files.

---
+++ BattleShipFive/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipFive
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[,] gameBoard = new string[10, 10];
+            string player = "X";
+            int player1BoatAmound = 0;
+
+            StartGame(gameBoard);
+            MakeBoard(gameBoard);
+            for (int i = 0; i < 3; i++)
+            {
+
+            }
+        }
+        //functions
+        private static void StartGame(string[,] gameBoard)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    gameBoard[i, j] = " ";
+                }
+            }
+
+        }
+        //draws the board
+        private static void MakeBoard(string[,] gameBoard)
+        {
+            Console.WriteLine("          1         2         3         4         5         6         7         8         9         10    ");
+            Console.Write("      ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("————————— ");
+            }
+            DrawDiagonalLines();
+            Console.WriteLine(" ");
+            Console.Write("  A  ");
+            for (int l = 0; l < 10; l++)
+            {
+                Console.Write("|         ");
+            }
+            Console.Write("|");
+            DrawDiagonalLines();
+            Console.WriteLine("");
+
+            Console.Write("      ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("————————— ");
+            }
+            DrawDiagonalLines();
+            Console.WriteLine(" ");
+            Console.Write("  B  ");
+            for (int l = 0; l < 10; l++)
+            {
+                Console.Write("|         ");
+            }
+            Console.Write("|");
+            DrawDiagonalLines();
+            Console.WriteLine("");
+
+            Console.Write("      ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("————————— ");
+            }
+            DrawDiagonalLines();
+            Console.WriteLine(" ");
+            Console.Write("  C  ");
+            for (int l = 0; l < 10; l++)
+            {
+                Console.Write("|         ");
+            }
+            Console.Write("|");
+            DrawDiagonalLines();
+            Console.WriteLine("");
+
+            Console.Write("      ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("————————— ");
+            }
+            DrawDiagonalLines();
+            Console.WriteLine(" ");
+            Console.Write("  D  ");
+            for (int l = 0; l < 10; l++)
+            {
+                Console.Write("|         ");
+            }
+            Console.Write("|");
+            DrawDiagonalLines();
+            Console.WriteLine("");
+
+            Console.Write("      ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("————————— ");
+            }
+            DrawDiagonalLines();
+            Console.WriteLine(" ");
+            Console.Write("  E  ");
+            for (int l = 0; l < 10; l++)
+            {
+                Console.Write("|         ");
+            }
+            Console.Write("|");
+            DrawDiagonalLines();
+            Console.WriteLine("");
+
+            Console.Write("      ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("————————— ");
+            }
+            DrawDiagonalLines();
+            Console.WriteLine(" ");
+            Console.Write("  F  ");
+            for (int l = 0; l < 10; l++)
+            {
+                Console.Write("|         ");
+            }
+            Console.Write("|");
+            DrawDiagonalLines();
+            Console.WriteLine("");
+
+            Console.Write("      ");
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write("————————— ");
+            }
+            DrawDiagonalLines();
+            Console.WriteLine(" ");
+            Console.Write("  G  ");
+            for (int l = 0; l < 10; l++)
+            {
+                Console.Write("|         ");
+            }
+            Console.Write("|");
+            DrawDiagonalLines();
+            Console.WriteLine("");
+
+        }
+        //draw diagonal line for board
+        private static void DrawDiagonalLines()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(" ");
+                Console.Write("     ");
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("|         ");
+                }
+                Console.Write("|");
+
+            }
+        }
+    }
+}
---
+++ BattleShipFive/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BattleShipFive")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSW DoE")]
+[assembly: AssemblyProduct("BattleShipFive")]
+[assembly: AssemblyCopyright("Copyright © NSW DoE 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a454e1a5-c8c4-4a82-8b4d-b49cde629b6c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
